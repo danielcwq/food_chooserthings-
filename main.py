df92b4dfd45403fb43a497fcbf31762dc8bddbda
danielcwq <75194564+danielcwq@users.noreply.github.com>
2020-12-01 11:35:38 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+
+food_list=["Ong Yong Lee", "AMK ZC 1", "AMK ZC 2", "Jai Thai", "Fish Soup Shunfu", "Chicken Rice Shunfu", "AMK Curry Fish Head", "Soi Thai Toa Payoh", "Jap Jln Pemp", "taipei", "NEX", "J8"]
+
+print(random.choice(food_list))
